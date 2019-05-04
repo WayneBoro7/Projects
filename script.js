c83e2477d8d16c468a5e626c6dc1194a7699ebce
Wayne Peterkin <wpeterkin8958@cimsnyc.org>
2019-05-04 12:16:01 +0000

    we finally fulfilled the purpose of the website by getting to give names of resturants

--- script.js
+++ script.js
@@ -19,12 +19,10 @@ showSlides();
 
 let apiKey = "c029f6c66913247ae5c214d8090e237d";
 
-
-
-
 let resSearch = function() {
     
-    let city = document.getElementById('city')
+    // gets city id
+    let city = document.getElementById("city").value;
     
     let locUrl = ("https://developers.zomato.com/api/v2.1/locations?query=" + city)
     
@@ -37,51 +35,59 @@ let resSearch = function() {
     
     
     locRequest.onload = function (){
-        let locData = JSON.parse(this.response)
+        let locData = JSON.parse(this.response);
         
-        console.log(locData)
+        let cityId = (locData.location_suggestions[0].city_id);
+        
+        let searchRequest = new XMLHttpRequest();
+        let searchUrl = "https://developers.zomato.com/api/v2.1/search?entity_id="+cityId+"&entity_type=city&count=20&cuisines=308";
+         let placeCounter = 0;
+        
+        
+        searchRequest.open("GET", searchUrl, true);
+        searchRequest.setRequestHeader("content-type", "application/json");
+        searchRequest.setRequestHeader("user-key", apiKey);
+    
+    
+        searchRequest.onload = function() {
+            let resData = JSON.parse(this.response);
+        
+            let restaurantList = document.getElementById('restaurantList');
+    
+            let row = null;
+           
+           
+           console.log(resData)
+           
+            if (locRequest.status >= 200 && locRequest.status < 400){
+                resData.restaurants.forEach(restaurant => {
+                    
+                    if (placeCounter % 2 == 0){
+                        row = document.createElement('div');
+                        row.className = "row"
+                        restaurantList.appendChild(row);
+                    }
+                    
+                    
+                    let place = document.createElement('div');
+                    place.className = "col-6";
+                    
+                    
+                    let p = document.createElement('p');
+                    p.textContent = restaurant.restaurant.name;
+                    
+                    place.appendChild(p);
+                    row.appendChild(place);
+                    
+                    placeCounter++;
+                })
+            }
+        };
+    
+        searchRequest.send();
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
     locRequest.send();
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // let searchUrl = "https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&cuisines=308";
-
-    // let searchRequest = new XMLHttpRequest();
-    
-    // searchRequest.open("GET", searchUrl, true);
-    // searchRequest.setRequestHeader("content-type", "application/json");
-    // searchRequest.setRequestHeader("user-key", apiKey);
-
-
-    // searchRequest.onload = function() {
-    //     let data = JSON.parse(this.response);
-    
-    //     console.log(data);
-    // };
-    
-    // searchRequest.send();
-    
-    
     
 }
 
